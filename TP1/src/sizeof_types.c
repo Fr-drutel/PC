df58043c2505c201dfeea41223fb16ed815552b6
Paul-dumont <paul.dumont@cpe.fr>
2024-09-16 09:18:43 +0200

    ex 3

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -8,10 +8,18 @@ int main(void)
 {
     
     printf("sizeof(char): %lu\n", sizeof (char));
+    printf("sizeof(signed char): %lu\n", sizeof (signed char));
+    printf("sizeof(unsigned char): %lu\n", sizeof (unsigned char));
+
     printf("sizeof(short): %lu\n", sizeof (short));
+    printf("sizeof(signed short): %lu\n", sizeof (signed short));
+
     printf("sizeof(int): %lu\n", sizeof (int));
+    printf("sizeof(signed int): %lu\n", sizeof (signed int));
+
     printf("sizeof(long int): %lu\n", sizeof (long int));
-    // pas compris pourquoi taille 8 et 8
+    printf("sizeof(signed long int): %lu\n", sizeof (signed long int));
+
     printf("sizeof(long long int): %lu\n", sizeof (long long int));
     printf("sizeof(float): %lu\n", sizeof (float));
     printf("sizeof(double): %lu\n", sizeof (double));
