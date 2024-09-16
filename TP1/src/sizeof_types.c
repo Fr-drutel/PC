b96c6e1970a86dfc3eaee7828e80c0a8447dabb3
Paul-dumont <paul.dumont@cpe.fr>
2024-09-16 09:06:54 +0200

    ex3

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -6,10 +6,15 @@
 
 int main(void)
 {
-    // pas compris pourquoi lu et pas c 
+    
     printf("sizeof(char): %lu\n", sizeof (char));
     printf("sizeof(short): %lu\n", sizeof (short));
-
-
+    printf("sizeof(int): %lu\n", sizeof (int));
+    printf("sizeof(long int): %lu\n", sizeof (long int));
+    // pas compris pourquoi taille 8 et 8
+    printf("sizeof(long long int): %lu\n", sizeof (long long int));
+    printf("sizeof(float): %lu\n", sizeof (float));
+    printf("sizeof(double): %lu\n", sizeof (double));
+    printf("sizeof(long double): %lu\n", sizeof (long double));
 
 }
