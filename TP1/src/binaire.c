c69b134024456a8935126de11be59db61036a414
Paul-dumont <paul.dumont@cpe.fr>
2024-09-16 11:58:35 +0200

    ex9

--- TP1/src/binaire.c
+++ TP1/src/binaire.c
@@ -7,6 +7,23 @@
 
 int main(void)
 {
+    int nombre = 7;
+    
+    
+    // faire modulo puis division du chiffre -1 puis avec les reste 
+    // des modulo on a le code binaire a l'enveres 
+    for(;;){
 
+    int reste = nombre % 2; 
+
+    if (reste == 1){
+        nombre--;
+    }
+
+    // stocker le reste
+
+    nombre /= 2;
+
+    }
 
 }
