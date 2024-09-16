c7f0232cda10c9f4aec263b087d16e1178dfb28a
Fr-drutel <francois-regis.drutel@cpe.fr>
2024-09-16 09:47:15 +0200

    bouclesses

---
+++ TP1/src/boucles.c
@@ -0,0 +1,12 @@
+/* Fichier: boucles.c
+* Objectif : utiliser des boucles `for` pour afficher un triangle rectangle
+* Auteurs: François-Régis Drutel et Paul Dumont
+*/
+
+#include <stdio.h> 
+
+int main(void)
+{
+
+
+}
