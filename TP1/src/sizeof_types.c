6051c08b997ab76f9370f6811026ae4d58c41351
Paul-dumont <paul.dumont@cpe.fr>
2024-09-16 09:01:20 +0200

    push ex3

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -2,9 +2,14 @@
 * affiche la taille en octets des différents types de base
 * auteur: François-Régis Drutel et Paul Dumont
 */
+#include <stdio.h> 
 
 int main(void)
 {
+    // pas compris pourquoi lu et pas c 
+    printf("sizeof(char): %lu\n", sizeof (char));
+    printf("sizeof(short): %lu\n", sizeof (short));
 
-    
+
+
 }
