a83d9c7ec888aa29a31d874dfbefa2ef442f8050
Paul-dumont <paul.dumont@cpe.fr>
2024-09-16 09:45:08 +0200

    ex 5

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -8,5 +8,31 @@
 int main(void)
 {
 
+    int a = 16;
+    int b = 3;
+
+    //1 
+    printf("addition : %i \n", a + b);
+
+    //2 
+    printf("soustraction : %i \n", a - b);
+
+    //3 
+    printf("multiplication : %i \n", a * b);
+
+    //4 
+    printf("division : %i \n", a / b);
+    
+    //5 
+    printf("modulo : %i \n", a % b);
+
+    //6
+    printf("egal? : %i \n", a == b);
+
+    //7
+    printf("supp? : %i \n", a > b);
+
+
+
 
 }
