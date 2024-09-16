662541bd1bb2a14a43eb41c13861af66ce8257a2
Fr-drutel <francois-regis.drutel@cpe.fr>
2024-09-16 08:34:34 +0200

    ex 1 FR

---
+++ TP1/src/cercle.c
@@ -0,0 +1,13 @@
+/* Fichier: bonjour.c
+* affiche Bonjour le Monde! à l'écran 
+* auteur: François-Régis Drutel
+* Ceci est un commentaire sur plusieurs lignes
+*/
+// cc yy
+
+#include <stdio.h> // headers
+
+int main(void) 
+{
+
+}
