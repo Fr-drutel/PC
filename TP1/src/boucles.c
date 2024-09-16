4afc9fc78fc164375b59a16bc6ff7c4d923f78e0
Paul-dumont <paul.dumont@cpe.fr>
2024-09-16 11:00:04 +0200

    epush Ex 6 7

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -7,6 +7,49 @@
 
 int main(void)
 {
+    int compteur = 5;
+    for (int ligne = 0;ligne < compteur-1;ligne++){
+        for (int col = 0;col <compteur;col++){
+            if(ligne == col){
+                printf("*\n");
+            }
+            else if(col == 0){
+                printf("*");
+            }
+            else if(col < ligne){
+                printf("#");
+            }
+        }
+    }
+    for (int fin = 0;fin < compteur;fin++){ 
+        printf("*");
+    }
+    printf("\n");
+}
 
+int main(void)
+{
+    int compteur = 117;
+    int ligne = 0;
 
+    while (ligne < compteur-1){
+        int col = 0;
+        while (col <compteur){
+            if(ligne == col){
+                printf("*\n");
+            }
+            else if(col == 0){
+                printf("*");
+            }
+            else if(col < ligne){
+                printf("#");
+            }
+        col++;
+        }
+    ligne++;
+    }
+    for (int fin = 0;fin < compteur;fin++){ 
+        printf("*");
+    }
+    printf("\n");
 }
---
+++ TP1/src/conditions.c
@@ -0,0 +1,32 @@
+/* Fichier: bonjour.c
+* Objectif : ...
+* Auteurs: François-Régis Drutel et Paul Dumont
+*/
+
+#include <stdio.h> 
+
+int main(void)
+{
+    somme = []
+    for(int i = 1 ; i < 1001 ; i++){
+
+        printf("%i \n", i);
+
+        if (i / 5 || i / 7){
+            i to somme
+        }
+        if (i / 11){
+            continue;
+        }
+
+        somme = somme i + ....
+
+        if (somme > 5000){
+            printf(somme)
+            break;
+        }
+        printf(somme)
+
+
+    }
+}
