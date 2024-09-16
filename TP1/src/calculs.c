b8135d347f37970229fa0649ebb83b5ab4f1ea4d
Paul-dumont <paul.dumont@cpe.fr>
2024-09-16 11:22:16 +0200

    debut ex 8

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,29 @@
+/* Fichier: bonjour.c
+* Objectif : afficher Bonjour le Monde! à l'écran 
+* Auteurs: François-Régis Drutel et Paul Dumont
+*/
 
+#include <stdio.h> 
+
+int main(void)
+{
+
+    int num1 = 7;
+    int num2 = 3;
+    char op = "+";
+
+    switch (op) {
+        case + : num1 + num2
+            break;
+        case - : num1 - num2
+            break;
+        case \n : num1 - num2
+            break;
+        case / : num1 / num2
+            break;
+        case % : num1 % num2
+            break;
+        
+}
+
+}
