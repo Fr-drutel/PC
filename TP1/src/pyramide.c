c86586f2715d1b6456e609b0f6ffca7339645126
Paul-dumont <paul.dumont@cpe.fr>
2024-09-20 08:26:23 +0200

    ex 10

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,33 @@
 
+/* Fichier: bonjour.c
+* Objectif : afficher Bonjour le Monde! à l'écran 
+* Auteurs: François-Régis Drutel et Paul Dumont
+*/
+
+#include <stdio.h> 
+
+int main(void)
+{ 
+
+    int n = 5;
+
+    for(int i = 0 ;i < 5;i++){
+        for(int j = 0;j < (n -i -1);j++){
+           printf(" ");
+        }
+
+        for(int j = 0;j < (i + 1);j++){
+           printf("%i",j + 1);
+        }
+
+        for(int j = i;j > 0;j--){
+           printf("%i",j);
+        }
+
+        for(int j = 0;j < (n -i -1);j++){
+           printf(" ");
+        }
+
+        printf("\n");
+    } 
+}
