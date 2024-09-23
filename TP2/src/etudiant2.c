7cabd83d58183308cf48b026ce32e22120ac8ef4
Fr-drutel <francois-regis.drutel@cpe.fr>
2024-09-23 11:17:12 +0200

    renommés

--- TP2/src/etudiant2.c
+++ TP2/src/etudiant2.c
@@ -4,6 +4,7 @@
 */
 
 #include <stdio.h> 
+#include <string.h>
 
 int main(void)
 {
@@ -19,43 +20,35 @@ struct etudiant{
 };
 struct etudiant etudiant[5];
 
-strcpy(etudiant[0].nom, "Pierre");
-strcpy(etudiant[0].prenom, "Dupont");
-strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
-etudiant[0].note.notes_en_C, 17,3;
-etudiant[0].note.notes_en_S, 12,9;
+strcpy(etudiant[0].nom, "Paul");
+strcpy(etudiant[0].prenom, "Dumont");
+strcpy(etudiant[0].adresse, "Meximieux");
+etudiant[0].note.notes_en_C, 20.0;
+etudiant[0].note.notes_en_S, 18.8;
 
-strcpy(etudiant[0].nom, "Pierre");
-strcpy(etudiant[0].prenom, "Dupont");
-strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
-etudiant[0].note.notes_en_C, 17,3;
-etudiant[0].note.notes_en_S, 12,9;
+strcpy(etudiant[0].nom, "Raphaël");
+strcpy(etudiant[0].prenom, "Berger");
+strcpy(etudiant[0].adresse, "Grasse");
+etudiant[0].note.notes_en_C, 19.7;
+etudiant[0].note.notes_en_S, 19.4;
 
-strcpy(etudiant[0].nom, "Pierre");
-strcpy(etudiant[0].prenom, "Dupont");
-strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
-etudiant[0].note.notes_en_C, 17,3;
-etudiant[0].note.notes_en_S, 12,9;
+strcpy(etudiant[0].nom, "Olivier");
+strcpy(etudiant[0].prenom, "Mermet");
+strcpy(etudiant[0].adresse, "Lyon");
+etudiant[0].note.notes_en_C, 17.4;
+etudiant[0].note.notes_en_S, 19.6;
 
-strcpy(etudiant[0].nom, "Pierre");
-strcpy(etudiant[0].prenom, "Dupont");
-strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
-etudiant[0].note.notes_en_C, 17,3;
-etudiant[0].note.notes_en_S, 12,9;
+strcpy(etudiant[0].nom, "Romain");
+strcpy(etudiant[0].prenom, "Thibault");
+strcpy(etudiant[0].adresse, "Villeurbanne");
+etudiant[0].note.notes_en_C, 16.2;
+etudiant[0].note.notes_en_S, 17.1;
 
-strcpy(etudiant[0].nom, "Pierre");
-strcpy(etudiant[0].prenom, "Dupont");
-strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
-etudiant[0].note.notes_en_C, 17,3;
-etudiant[0].note.notes_en_S, 12,9;
-
-
-
-
-
-
-
-
+strcpy(etudiant[0].nom, "Adonis");
+strcpy(etudiant[0].prenom, "Robin");
+strcpy(etudiant[0].adresse, "Villeurbanne");
+etudiant[0].note.notes_en_C, 18.6;
+etudiant[0].note.notes_en_S, 20.0;
 
 }
 
