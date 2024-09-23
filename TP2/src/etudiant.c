41af2b030953cdfef87e6c1ea25a7ecd6142d4b4
Fr-drutel <francois-regis.drutel@cpe.fr>
2024-09-23 11:08:31 +0200

    ex 5

--- TP2/src/etudiant.c
+++ TP2/src/etudiant.c
@@ -1,5 +1,5 @@
 /* Fichier: etudiant.c
-* Objectif : gèrer les informations de cinq étudiant.e.s en utilisant des tableaux
+* Objectif : gérer les informations de cinq étudiant.e.s en utilisant des tableaux
 * Auteurs: François-Régis Drutel et Paul Dumont
 */
 
@@ -7,6 +7,20 @@
 
 int main(void)
 {
-
+    char nom[5][25] = {"Paul Dumont","Raphaël Berger","Olivier Mermet","Romain Thibault","Adonis Robin"};
+    char adresse[5][25] = {"Meximieux","Grasse","Lyon","Villeurbanne","Villeurbanne"};
+    float note_prog_c[5] = {20.0,19,17,16,18};
+    float note_sys_exploit[5] = {18,19,19,17,20};
+    
+    for(int i = 0; i < 5; i++){
+        printf("Etudiant : %s\n", nom[i]);
+        printf("Adresse : %s\n", adresse[i]);
+        printf("Note en Programmation :%.1f\n", note_prog_c[i]);
+        printf("Note en système : %.1f\n", note_sys_exploit[i]);
+        printf("\n");
+    }
+       
 
 }
+
+
