fb1a59bcac391073caab39bea02ff1d9412b3028
Paul-dumont <paul.dumont@cpe.fr>
2024-09-23 11:08:27 +0200

    ex ex6 tp2

--- TP2/src/etudiant2.c
+++ TP2/src/etudiant2.c
@@ -8,5 +8,50 @@
 int main(void)
 {
 
+struct etudiant{
+    char nom[30];
+    char prenom[30];
+    char adresse[30];
+    struct note{
+        float notes_en_C[30];
+        float notes_en_S[30];
+    } note;
+};
+struct etudiant etudiant[5];
+
+
+strcpy(etudiant[0].nom, "Pierre");
+strcpy(etudiant[0].prenom, "Dupont");
+strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
+etudiant[0].note.notes_en_C, 17,3;
+etudiant[0].note.notes_en_S, 12,9;
+
+strcpy(etudiant[0].nom, "Pierre");
+strcpy(etudiant[0].prenom, "Dupont");
+strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
+etudiant[0].note.notes_en_C, 17,3;
+etudiant[0].note.notes_en_S, 12,9;
+
+strcpy(etudiant[0].nom, "Pierre");
+strcpy(etudiant[0].prenom, "Dupont");
+strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
+etudiant[0].note.notes_en_C, 17,3;
+etudiant[0].note.notes_en_S, 12,9;
+
+strcpy(etudiant[0].nom, "Pierre");
+strcpy(etudiant[0].prenom, "Dupont");
+strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
+etudiant[0].note.notes_en_C, 17,3;
+etudiant[0].note.notes_en_S, 12,9;
+
+
+
+
+
+
+
+
 
 }
+
+
