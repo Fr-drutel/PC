5b5dd6a8fe99751d46a05510ce52cc8ebd96a55e
Paul-dumont <paul.dumont@cpe.fr>
2024-09-23 11:09:01 +0200

    EX -6 TP 2

--- TP2/src/etudiant2.c
+++ TP2/src/etudiant2.c
@@ -19,6 +19,11 @@ struct etudiant{
 };
 struct etudiant etudiant[5];
 
+strcpy(etudiant[0].nom, "Pierre");
+strcpy(etudiant[0].prenom, "Dupont");
+strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
+etudiant[0].note.notes_en_C, 17,3;
+etudiant[0].note.notes_en_S, 12,9;
 
 strcpy(etudiant[0].nom, "Pierre");
 strcpy(etudiant[0].prenom, "Dupont");
