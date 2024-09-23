a8b3c4fb30cd25db867f81c977618887aa5452e9
Paul-dumont <paul.dumont@cpe.fr>
2024-09-23 08:56:32 +0200

    ex 3 tp2

--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -7,6 +7,19 @@
 
 int main(void)
 {
+    int n = 7; 
+
+    int tab[n];
+    tab[0] = 0;
+    tab[1] = 1;
+
+    for(int i=2;i < n;i++){
+        tab[i] = tab[i-1] + tab[i-2];
+    }
+
+    for(int i=0;i < n;i++){
+        printf("%i\n",tab[i]);
+    }
 
 
 }
