c4143c144c6d9cf07bf82f3974d4c9e82a4fcc68
Paul-dumont <paul.dumont@cpe.fr>
2024-09-23 11:25:46 +0200

    push

--- TP2/src/etudiant2.c
+++ TP2/src/etudiant2.c
@@ -23,38 +23,40 @@ struct etudiant etudiant[5];
 strcpy(etudiant[0].nom, "Pierre");
 strcpy(etudiant[0].prenom, "Dupont");
 strcpy(etudiant[0].adresse, "Boulevard du 11 novembre 1918");
-etudiant[0].note.notes_en_C, 17,3;
-etudiant[0].note.notes_en_S, 12,9;
+etudiant[0].note.notes_en_C, 17.3;
+etudiant[0].note.notes_en_S, 12.9;
 
 strcpy(etudiant[1].nom, "dezd");
 strcpy(etudiant[1].prenom, "Dupont");
 strcpy(etudiant[1].adresse, "Boulevard du 11 novembre 1918");
-etudiant[1].note.notes_en_C, 17,3;
-etudiant[1].note.notes_en_S, 12,9;
+etudiant[1].note.notes_en_C, 17.3;
+etudiant[1].note.notes_en_S, 12.9;
 
 strcpy(etudiant[2].nom, "zefef");
 strcpy(etudiant[2].prenom, "Dupont");
 strcpy(etudiant[2].adresse, "Boulevard du 11 novembre 1918");
-etudiant[2].note.notes_en_C, 17,3;
-etudiant[2].note.notes_en_S, 12,9;
+etudiant[2].note.notes_en_C, 17.3;
+etudiant[2].note.notes_en_S, 12.9;
 
 strcpy(etudiant[3].nom, "Pierre");
 strcpy(etudiant[3].prenom, "Dupont");
 strcpy(etudiant[3].adresse, "Boulevard du 11 novembre 1918");
-etudiant[3].note.notes_en_C, 17,3;
-etudiant[3].note.notes_en_S, 12,9;
+etudiant[3].note.notes_en_C, 17.3;
+etudiant[3].note.notes_en_S, 12.9;
 
 strcpy(etudiant[4].nom, "fzefzefezf");
 strcpy(etudiant[4].prenom, "Dupont");
 strcpy(etudiant[4].adresse, "Boulevard du 11 novembre 1918");
-etudiant[4].note.notes_en_C, 17,3;
-etudiant[4].note.notes_en_S, 12,9;
+etudiant[4].note.notes_en_C = 17.3;
+etudiant[4].note.notes_en_S =  12.9;
 
 for(int i = 0; i < 5; i++){
-    printf("mon nom : %s\n",etudiant[i].nom);
-    printf("%s\n",etudiant[i].prenom);
-    printf("%s\n",etudiant[i].adresse);
-    printf("%s\n",etudiant[i].nom);
+    printf("mon nom est : %s\n",etudiant[i].nom);
+    printf("mon prenom est : %s\n",etudiant[i].prenom);
+    printf("mon adresse est : %s\n",etudiant[i].adresse);
+    printf("ma note de C %.1f\n",etudiant[i].note.notes_en_C);
+    printf("ma note de S %.1f\n",etudiant[i].note.notes_en_S);
+    printf("\n");
 }
 
 
