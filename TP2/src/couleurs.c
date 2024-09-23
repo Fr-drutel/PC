c19743dd444060c761ba15867ad3b491029ce241
Paul-dumont <paul.dumont@cpe.fr>
2024-09-23 11:42:35 +0200

    PPDZH8AUIBUIONAZF

--- TP2/src/couleurs.c
+++ TP2/src/couleurs.c
@@ -7,6 +7,51 @@
 
 int main(void)
 {
+struct couleur{
+    int rouge;
+    int vert;
+    int bleu;
+    int alpha;
+};
+struct couleur couleur[10];
 
+couleur[0].rouge = 0xef;
+couleur[0].vert = 0xef;
+couleur[0].bleu = 0xef;
+couleur[0].alpha = 0xef;
 
+couleur[1].rouge = 0xef;
+couleur[1].vert = 0xef;
+couleur[1].bleu = 0xef;
+couleur[1].alpha = 0xef;
+
+couleur[2].rouge = 0xef;
+couleur[2].vert = 0xef;
+couleur[2].bleu = 0xef;
+couleur[2].alpha = 0xef;
+
+couleur[3].rouge = 0xef;
+couleur[3].vert = 0xef;
+couleur[3].bleu = 0xef;
+couleur[3].alpha = 0xef;
+
+couleur[0].rouge = 0xef;
+couleur[0].vert = 0xef;
+couleur[0].bleu = 0xef;
+couleur[0].alpha = 0xef;
+
+couleur[0].rouge = 0xef;
+couleur[0].vert = 0xef;
+couleur[0].bleu = 0xef;
+couleur[0].alpha = 0xef;
+
+couleur[0].rouge = 0xef;
+couleur[0].vert = 0xef;
+couleur[0].bleu = 0xef;
+couleur[0].alpha = 0xef;
+
+couleur[0].rouge = 0xef;
+couleur[0].vert = 0xef;
+couleur[0].bleu = 0xef;
+couleur[0].alpha = 0xef;
 }
