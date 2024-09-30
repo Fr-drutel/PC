5cab3977de108740892e8c28b019590f26e74288
Fr-drutel <francois-regis.drutel@cpe.fr>
2024-09-30 09:28:03 +0200

    headers

--- TP3/src/tri.c
+++ TP3/src/tri.c
@@ -1,5 +1,6 @@
-/* Fichier: bonjour.c
-* Objectif : afficher Bonjour le Monde! à l'écran 
+/* Fichier: tri.c
+* Objectif : créer un tableau de 100 entiers, 
+* puis trier ces entiers par ordre croissant.
 * Auteurs: François-Régis Drutel et Paul Dumont
 */
 
