288775bf667d45c5f0d6b6a2a5fa29ceacd4dbf4
Fr-drutel <francois-regis.drutel@cpe.fr>
2024-09-30 08:35:52 +0200

    grand_petit h

--- TP3/src/grand_petit.c
+++ TP3/src/grand_petit.c
@@ -1,5 +1,6 @@
-/* Fichier: bonjour.c
-* Objectif : afficher Bonjour le Monde! à l'écran 
+/* Fichier: grand_petit.c
+* Objectif : créer un tableau de 100 entiers et calculer à la fois le 
+* numéro le plus grand et le numéro le plus petit dans ce tableau.
 * Auteurs: François-Régis Drutel et Paul Dumont
 */
 
