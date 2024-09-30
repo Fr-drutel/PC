735563ef6c2d3a96ee958728b4d9d8b96a7fd05b
Paul-dumont <paul.dumont@cpe.fr>
2024-09-30 09:46:03 +0200

    ex4

--- TP3/src/chercher.c
+++ TP3/src/chercher.c
@@ -14,10 +14,30 @@ srand(time(0));
 int tab_int[100];
 int *ptr = &tab_int[0];
 
-printf("tableau non trié :\n");
-for(int i = 0; i <101;i++){
+printf("tableau :\n");
+for(int i = 0; i <100;i++){
     *ptr = rand() % 100 + 1;
     printf("%i ",*ptr);
     ptr++;
 }
+ptr = tab_int;
+
+int num_user;
+printf("\nun entier ? \n");
+scanf("%i",&num_user);
+
+int present = 0;
+for(int i = 0; i <100;i++){
+    if(num_user == *ptr){
+        printf("l'entier %i est présent \n",num_user);
+        present = 1;
+        break;
+    }
+    ptr++;
+}
+
+if(present == 0){
+    printf("l'entier %i n'est pas présent \n",num_user);
+}
+
 }
