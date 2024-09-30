6f8fdb7332048315250563c7208b515e814250bc
Paul-dumont <paul.dumont@cpe.fr>
2024-09-30 08:21:54 +0200

    fin ex 1 tp3

--- TP3/src/sizeof.c
+++ TP3/src/sizeof.c
@@ -7,7 +7,15 @@
 
 int main(void)
 {
-printf("La taille de int est : %lu\n", sizeof (char)); 
-printf("La taille de int est : %lu\n", sizeof (char*)); 
-printf("La taille de int est : %lu\n", sizeof (char**)); 
+printf("La taille de int est : %lu octets\n", sizeof (int)); 
+printf("La taille de int* est : %lu octets\n", sizeof (int*)); 
+printf("La taille de int** est : %lu octets\n", sizeof (int**)); 
+
+printf("La taille de char* est : %lu octets\n", sizeof (char*)); 
+printf("La taille de char** est : %lu octets\n", sizeof (char**)); 
+printf("La taille de char*** est : %lu octets\n", sizeof (char***)); 
+
+printf("La taille de float* est : %lu octets\n", sizeof (float*)); 
+printf("La taille de float** est : %lu octets\n", sizeof (float**)); 
+printf("La taille de float*** est : %lu octets\n", sizeof (float***)); 
 }
