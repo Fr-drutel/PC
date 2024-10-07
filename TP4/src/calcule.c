38f5ab2df7e86495b911d9d9bade0c6a696188a7
Fr-drutel <francois-regis.drutel@cpe.fr>
2024-10-07 11:04:41 +0200

    headers

--- TP4/src/calcule.c
+++ TP4/src/calcule.c
@@ -1,5 +1,7 @@
-/* Fichier: bonjour.c
-* Objectif : afficher Bonjour le Monde! à l'écran 
+/* Fichier: calcule.c
+* Objectif : créer une calculatrice en ligne de commande permettant 
+* à l'utilisateur d'exécuter des opérations arithmétiques et logiques 
+* entre deux nombres à l'aide de divers opérateurs.
 * Auteurs: François-Régis Drutel et Paul Dumont
 */
 
