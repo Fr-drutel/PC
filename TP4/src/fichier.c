716330fd5abe01f29dfe06c00a7239a05c0292ad
Paul-dumont <paul.dumont@cpe.fr>
2024-10-07 09:46:33 +0200

    debut 2

---
+++ TP4/src/fichier.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int lire_fichier(char* 'nom_de_fichier');
+    char content[1000];
+    int fd, count, size;
+    fd = open ("./file.c", O_RDONLY);
+    size = read(fd, content, 1000);
+    for (count = 0; count < size; count ++) {
+    printf("%c", content[count]);
+    }
+    close(fd);
+    return 0;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int ecrire_dans_fichier(char* 'nom_de_fichier',char* 'message');
--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,39 +1,58 @@
 #include <stdio.h> 
-#include "operator.h"
+#include "fichier.h"
 
 int main(void)
 {
-    int num1 = 7;
-    int num2 = 2;
-    char op = '~';
-    int result = -1;
+    int ex = '1';
+    switch(ex){
+        case '1':
 
-    switch (op){
-        case '+' : 
-            result = somme(num1, num2);
+            int num1, num2;
+            char op;
+
+            printf("Entrez un entier pour num1 : ");
+            scanf("%d", &num1);
+
+            printf("Entrez un entier pour num2 : ");
+            scanf("%d", &num2);
+
+            printf("Entrez une opération (par exemple +, -, *, /, %%, &, |, ~) : ");
+            scanf(" %c", &op);
+
+            int result = -1;
+
+            switch (op){
+                case '+' : 
+                    result = somme(num1, num2);
+                    break;
+                case '-' : 
+                    result = difference(num1, num2);
+                    break;
+                case '*' : 
+                    result = produit(num1, num2);
+                    break;
+                case '/' : 
+                    result = divise(num1, num2);
+                    break;
+                case '%' : 
+                    result = modulo(num1, num2);
+                    break;
+                case '&' : 
+                    result = et(num1, num2);
+                    break;
+                case '|' : 
+                    result = ou(num1, num2);
+                    break;
+                case '~' : 
+                    result = negation(num1);
+                    break;
+            }
+
+            printf("resultat %c : %i \n", op, result);
             break;
-        case '-' : 
-            result = difference(num1, num2);
+        case '2':   
+
             break;
-        case '*' : 
-            result = produit(num1, num2);
-            break;
-        case '/' : 
-            result = divise(num1, num2);
-            break;
-        case '%' : 
-            result = modulo(num1, num2);
-            break;
-        case '&' : 
-            result = et(num1, num2);
-            break;
-        case '|' : 
-            result = ou(num1, num2);
-            break;
-        case '~' : 
-            result = negation(num1);
-            break;
+
     }
-
-    printf("resultat %c : %i \n", op, result);
 }
