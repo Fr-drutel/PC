75dab98b9bf8d2742f18ed722c0426d24e44ba5b
Paul-dumont <paul.dumont@cpe.fr>
2024-10-21 08:07:27 +0200

    DEbut tp6

--- TP5/src/repertoire.c
+++ TP5/src/repertoire.c
@@ -48,19 +48,50 @@ int lire_dossier_recursif(char* nom_repertoire) {
     }
     closedir(dirp);
 }
+int lire_dossier_iteratif(char* nom_repertoire) {
+    // Création d'une pile (tableau dynamique) pour stocker les répertoires
+    char** pile = (char**)malloc(100 * sizeof(char*));
+    int taille_pile = 0;
 
-int lire_dossier_eteratif(char* nom_repertoire) {
-    printf("%s",nom_repertoire);
-    DIR *dirp = opendir(nom_repertoire);
-    struct dirent * ent;
-    if (ent == NULL) {
-            break;
-        }
-    if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
+    // Pousser le répertoire de départ sur la pile
+    pile[taille_pile] = strdup(nom_repertoire);
+    taille_pile++;
+
+    while (taille_pile > 0) {
+        // Pop de la pile
+        char* rep_courant = pile[taille_pile - 1];
+        taille_pile--;
+
+        printf("%s\n", rep_courant);
+        DIR *dirp = opendir(rep_courant);
+        if (dirp == NULL) {
+            perror("Erreur d'ouverture de dossier");
+            free(rep_courant);
             continue;
         }
 
-        // a continuer 
+        struct dirent *ent;
+        while ((ent = readdir(dirp)) != NULL) {
+            if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) {
+                continue;
+            }
 
+            char chemin[1024];
+            sprintf(chemin, "%s/%s", rep_courant, ent->d_name);
+            printf("%s\n", chemin);
 
+            if (ent->d_type == DT_DIR) {
+                // Pousser le sous-dossier sur la pile
+                pile[taille_pile] = strdup(chemin);
+                taille_pile++;
+            }
+        }
+
+        closedir(dirp);
+        free(rep_courant);
+    }
+
+    // Libération de la mémoire
+    free(pile);
+    return 0;
 }
--- TP5/src/repertoire.h
+++ TP5/src/repertoire.h
@@ -1,2 +1,3 @@
 int lire_dossier(char*);
 int lire_dossier_recursif(char*);
+int lire_dossier_iteratif(char*);
